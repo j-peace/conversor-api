081b417f1fe1715d4145944e0e1161e9bf6c8c4e
j-peace <tiagopaz.hs@gmail.com>
2025-06-20 19:40:51 -0300

    feat: endpoint /api/ping em Node.js para Vercel

--- app.rb
+++ app.rb
@@ -2,31 +2,27 @@ require 'sinatra/base'
 require 'json'
 
 class ApiApp < Sinatra::Base
-  # Desabilita TODAS as proteções de segurança do Rack::Protection.
-  # Esta é a correção definitiva para o erro "Host not permitted".
-  disable :protection
-
-  # Configurações do servidor
-  configure do
-    set :bind, '0.0.0.0'
+  # set host authorization to all environments
+  set :host_authorization, { permitted_hosts: [] }
+  configure :development do
+    set :host_authorization, { permitted_hosts: [] }
+  end
+  configure :production do
+    set :host_authorization, { permitted_hosts: [".fly.dev"] }
   end
 
-  # Endpoint para listar unidades
+  get '/' do
+    content_type :json
+    { message: 'API em Ruby wiht Sinatra is running!' }.to_json
+  end
+
   get '/list_of_units' do
     content_type :json
     { list_of_units: ['k', 'f', 'c'] }.to_json
   end
 
-  # Endpoint para dobrar o valor
-  get '/dobrarValor' do
+  get '/conversion' do
     content_type :json
-    valor = params['valor'].to_i
-    { resultado: valor * 2 }.to_json
-  end
-
-  # Endpoint raiz para verificar se a API está no ar
-  get '/' do
-    content_type :json
-    { message: 'API em Ruby com Sinatra está no ar!' }.to_json
+    { converted_temp: params['temperatureInCelsius'] + 1 }.to_json
   end
 end 
